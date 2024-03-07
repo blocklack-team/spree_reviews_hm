0c7cfce38b945b6671dbffd836d8b58102f163d1
Alejandro Sanchez <>
2024-03-06 23:39:26 -0500

    update array

--- config/initializers/spree_permitted_attributes.rb
+++ config/initializers/spree_permitted_attributes.rb
@@ -3,6 +3,7 @@ module Spree
     ATTRIBUTES += %i[review_attributes feedback_review_attributes]
     mattr_reader *ATTRIBUTES
 
+    @product_attributes ||= []
     @product_attributes += %i[avg_rating reviews_count]
     @review_attributes = [:rating, :title, :review, :name, :show_identifier]
     @feedback_review_attributes = [:rating, :comment]
