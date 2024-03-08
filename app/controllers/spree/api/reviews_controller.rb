ba530cc21b704925a9744fec2533084b4e490e79
Alejandro Sanchez <>
2024-03-08 18:30:01 -0500

    update api

--- app/controllers/spree/api/reviews_controller.rb
+++ app/controllers/spree/api/reviews_controller.rb
@@ -1,119 +1,86 @@
-# frozen_string_literal: true
+# app/controllers/spree/api/reviews_controller.rb
 
-class Spree::Api::ReviewsController < Spree::StoreController
-    helper Spree::BaseHelper
-    before_action :load_product, only: [:index, :new, :create, :edit, :update]
+module Spree
+  module Api
+    class ReviewsController < ApplicationController
+      before_action :load_product, only: %i[index new create edit update]
 
-		def index
-			@reviews = collection
-	
-			respond_to do |format|
-				format.html
-				format.json { render json: @reviews }
-			end
-		end
-  
-		def show
-			@approved_reviews = Spree::Review.default_approval_filter.where(product: @product)
-	
-			respond_to do |format|
-				format.html
-				format.json { render json: @approved_reviews }
-			end
-		end
-  
-		def new
-			@review = Spree::Review.new(product: @product)
-			authorize! :create, @review
-	
-			respond_to do |format|
-				format.html
-				format.json { render json: @review }
-			end
-		end
-  
-		def edit
-			@review = Spree::Review.find(params[:id])
-			if @review.product.nil?
-				flash[:error] = I18n.t('spree.error_no_product')
-			end
-			authorize! :update, @review
-	
-			respond_to do |format|
-				format.html
-				format.json { render json: @review }
-			end
-		end
-  
-    # save if all ok
-    def create
-      review_params[:rating].sub!(/\s*[^0-9]*\z/, '') if review_params[:rating].present?
-  
-      @review = Spree::Review.new(review_params)
-      @review.product = @product
-      @review.user = spree_current_user if spree_user_signed_in?
-      @review.ip_address = request.remote_ip
-      @review.locale = I18n.locale.to_s if Spree::Reviews::Config[:track_locale]
-      # Handle images
-      params[:review][:images]&.each do |image|
-        @review.images.new(attachment: image) if image.present?
-      end
-  
-      authorize! :create, @review
-      if @review.save
-        flash[:notice] = I18n.t('spree.review_successfully_submitted')
-        redirect_to spree.product_path(@product)
-      else
-        render :new
+      def index
+        @reviews = collection
+        render json: @reviews
       end
 
-			respond_to do |format|
-				format.html
-				format.json { render json: @review }
-			end
-    end
-  
-    def update
-      review_params[:rating].sub!(/\s*[^0-9]*\z/, '') if params[:review][:rating].present?
-  
-      @review = Spree::Review.find(params[:id])
-  
-      # Handle images
-      params[:review][:images]&.each do |image|
-        @review.images.new(attachment: image) if image.present?
-      end
-  
-      authorize! :update, @review
-      if @review.update(review_params)
-        flash[:notice] = I18n.t('spree.review_successfully_submitted')
-        redirect_to spree.product_path(@product)
-      else
-        render :edit
+      def show
+        @review = Spree::Review.find(params[:id])
+        render json: @review
       end
 
-			respond_to do |format|
-				format.html
-				format.json { render json: @review }
-			end
-    end
-  
-    private
+      def new
+        @review = Spree::Review.new(product: @product)
+        authorize! :create, @review
+        render json: @review
+      end
 
-		def collection
-			params[:q] ||= {}
-			@search = Spree::Review.ransack(params[:q])
-			@collection = Spree::Review.includes([:product, :user, :feedback_reviews])
-		end
-  
-    def load_product
-      @product = Spree::Product.friendly.find(params[:product_id])
-    end
-  
-    def permitted_review_attributes
-      [:rating, :title, :review, :name, :show_identifier, :images]
-    end
-  
-    def review_params
-      params.require(:review).permit(permitted_review_attributes)
+      def create
+        @review = Spree::Review.new(review_params)
+        @review.product = @product
+        @review.user = spree_current_user if spree_user_signed_in?
+        @review.ip_address = request.remote_ip
+        @review.locale = I18n.locale.to_s if Spree::Reviews::Config[:track_locale]
+
+        authorize! :create, @review
+
+        if @review.save
+          render json: @review, status: :created
+        else
+          render json: { errors: @review.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def edit
+        @review = Spree::Review.find(params[:id])
+        authorize! :update, @review
+        render json: @review
+      end
+
+      def update
+        @review = Spree::Review.find(params[:id])
+
+        authorize! :update, @review
+
+        if @review.update(review_params)
+          render json: @review
+        else
+          render json: { errors: @review.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @review = Spree::Review.find(params[:id])
+        authorize! :destroy, @review
+        @review.destroy
+        head :no_content
+      end
+
+      private
+
+      def collection
+        params[:q] ||= {}
+        @search = Spree::Review.ransack(params[:q])
+        @collection = @search.result.includes([:product, :user, :feedback_reviews])
+      end
+
+      def load_product
+        @product = Spree::Product.friendly.find(params[:product_id])
+      end
+
+      def permitted_review_attributes
+        %i[rating title review name show_identifier images]
+      end
+
+      def review_params
+        params.require(:review).permit(permitted_review_attributes)
+      end
     end
   end
+end
